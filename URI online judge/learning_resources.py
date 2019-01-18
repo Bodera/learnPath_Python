c82219e6794aecb7e0fd529b86a8213660ae20fb
Bode <rafaelrafa990@gmail.com>
2019-01-18 11:35:08 -0200

    First commit. (4 ejercicios)
    Como siempre, lo primero commit crea la nueva branch ~~-dev~~. Todavía no actualizo el README. Las soluciones fueron probadas y aceptadas, excepto la 1004.

---
+++ URI online judge/learning_resources.py
@@ -0,0 +1,14 @@
+#Site do python para praticar
+#https://wiki.python.org.br/ListaDeExercicios
+#https://wiki.python.org.br/CookBook
+#https://wiki.python.org.br/AprendaMais
+#https://python.org.br/introducao/
+#https://python.org.br/ferramentas/
+# PEP8 https://www.python.org/dev/peps/pep-0008/
+#http://ninja-ide.org
+#opencv books
+#https://docs.python.org/3.0/whatsnew/3.0.html
+
+
+#https://blog.academiain1.com.br/big-data-voce-conhece-os-4-tipos-de-analise-de-dados/
+#
---
+++ URI online judge/uri_1001.py
@@ -0,0 +1,31 @@
+#Autor: Rafael, el 18 de Enero de 2019.
+"""
+E X T R E M A D A M E N T E   B Á S I C O
+
+Lea dos variables, llamadas A y B, y haga la suma de estas dos variables, 
+ asignando su resultado a la variable X.
+Imprima X como se muestra. Imprimir la línea final después del resultado,
+ de lo contrario obtendrá "Error de presentación".
+
+Entrada:
+El archivo de entrada contendrá dos números enteros.
+15 
+-7 
+
+Salida:
+Imprima la letra X (mayúscula) con un espacio en blanco antes y después de
+ la señal igual seguida del valor de X, de acuerdo con el ejemplo.
+X = 8
+
+No se olvida de saltar a línea después del final del programa.
+"""
+#Declarando las variables.
+#Por convención, las constantes se declaran utilizando mayúsculas.
+#Para las variables se utilizan minúsculas.
+
+z = int(input())
+y = int(input())
+
+respuesta = z + y
+
+print("X =",respuesta)
