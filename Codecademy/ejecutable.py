25502b02eee42e1fb2f736244c5b3316831c9022
Bodera <rafaelrafa990@gmail.com>
2019-07-22 14:02:21 -0300

    Up battleship lesson.

--- Codecademy/ejecutable.py
+++ Codecademy/ejecutable.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-address = ["Flat Floor Street", "18", "New York"]
-pins = { "Mike": 1123, "Jow": 4468, "Jack Tequila": 0}
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-print(address[0], address[1])
+#range(5) es un acceso directo para range(0, 5)
+board = [] #crea una cuadrícula de 5x5 con todos los "O"s, para "océano".
 
-pin = int(input("Enter your pin: "))
+for O in range(5): #horizontal 
+	board.append(5 * ["O"]) #vertical
+	#board.append(5 * " O  ")
 
-def find_in_file(f):
-	myFile = open("sample.txt", "r")
-	fruits = myFile.read()
-	#fruits = fruits.splitlines()
-	if f in fruits:
-		return "That fruit is in the list."
-	else:
-		return "No such fruit found!"
+for value in board: 
+	print(value) #proba
 
-if pin in pins.values():
-	fruit = input("Enter a fruit: ")
-	print(find_in_file(fruit))
-else:
-	print("Incorrect pin.")
-	print("This info can only be accessed by: ")
-	for key in pins.keys():
-		print(key)
+#print(board)
+print_board(board_in):
+	for value in board:
+		print(value)
